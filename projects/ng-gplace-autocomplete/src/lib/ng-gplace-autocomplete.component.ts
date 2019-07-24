f53481d12bc20d81e3887fc47b6b76e3a97fb15a
atheodosiou <anastasios.theodosiou@gmail.com>
2019-07-24 22:43:54 +0300

    Style input

--- projects/ng-gplace-autocomplete/src/lib/ng-gplace-autocomplete.component.ts
+++ projects/ng-gplace-autocomplete/src/lib/ng-gplace-autocomplete.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ng-autocomplete',
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class NgAutocompleteComponent implements OnInit {
 
   constructor() { }
-
+  @Input() style:any;
+  @Input() placeholder:string = 'Search an address'
   ngOnInit() {
+    console.log(this.style) 
   }
 
 }
--- projects/ng-gplace-autocomplete/src/lib/ng-gplace-autocomplete.module.ts
+++ projects/ng-gplace-autocomplete/src/lib/ng-gplace-autocomplete.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { NgAutocompleteComponent } from './ng-gplace-autocomplete.component';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
 
 
 @NgModule({
   declarations: [NgAutocompleteComponent],
   imports: [
+    CommonModule,
+    FormsModule
   ],
   exports: [NgAutocompleteComponent]
 })

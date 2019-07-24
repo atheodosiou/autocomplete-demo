60cf8ada99e01163735480f9da2f221dd95de922
atheodosiou <anastasios.theodosiou@gmail.com>
2019-07-24 23:47:27 +0300

    adds

--- projects/ng-gplace-autocomplete/src/lib/ng-gplace-autocomplete.component.ts
+++ projects/ng-gplace-autocomplete/src/lib/ng-gplace-autocomplete.component.ts
@@ -1,17 +1,43 @@
-import { Component, OnInit, Input } from '@angular/core';
+
+import { Component, OnInit, Input, Output, ViewChild, EventEmitter, AfterViewInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'ng-autocomplete',
-  templateUrl:'./ng-gplace-autocomplete.component.html',
-  styleUrls:['./ng-gplace-autocomplete.component.scss']
+  templateUrl: './ng-gplace-autocomplete.component.html',
+  styleUrls: ['./ng-gplace-autocomplete.component.scss']
 })
-export class NgAutocompleteComponent implements OnInit {
+export class NgAutocompleteComponent implements OnInit, AfterViewInit {
 
   constructor() { }
-  @Input() style:any;
-  @Input() placeholder:string = 'Search an address'
+  @Input() style: any;
+  @Input() placeholder: string = 'Search an address'
+  @Input() adressType: string;
+  @Output() setAddress: EventEmitter<any> = new EventEmitter();
+  // @ViewChild('autocomplete') addresstext: any;
+  autocompleteInput: string;
+  queryWait: boolean;
+
   ngOnInit() {
-    console.log(this.style) 
+    console.log(this.style)
+  }
+  ngAfterViewInit() {
+    // this.getPlaceAutocomplete();
   }
 
+//   private getPlaceAutocomplete() {
+//     const autocomplete = new google.maps.places.Autocomplete(this.addresstext.nativeElement,
+//       {
+//         componentRestrictions: { country: 'US' },
+//         types: [this.adressType]  // 'establishment'|'address'|'geocode'
+//       });
+//     google.maps.event.addListener(autocomplete, 'place_changed', () => {
+//       const place = autocomplete.getPlace();
+//       this.invokeEvent(place);
+//     });
+//   }
+
+//   invokeEvent(place: Object) {
+//     this.setAddress.emit(place);
+// }
 }
